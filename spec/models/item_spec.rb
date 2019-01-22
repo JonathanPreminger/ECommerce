4d1285b7202468448df73fa46f02131b168ac31f
ssimonlp <simon.lepavec@gmail.com>
2019-01-22 21:09:59 +0100

    fix rubocop issues on Item  model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: items
+#
+#  id                  :bigint(8)        not null, primary key
+#  name                :string
+#  image_url           :string
+#  description         :text
+#  price               :decimal(7, 2)    not null
+#  has_discount        :boolean          default(FALSE)
+#  male                :boolean
+#  female              :boolean
+#  discount_percentage :integer          default(0)
+#  category_id         :bigint(8)
+#  brand_id            :bigint(8)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
