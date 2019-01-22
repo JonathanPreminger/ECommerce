2e6c856b43b6d0c49ae59b1aae71cbbfc0b28813
ssimonlp <simon.lepavec@gmail.com>
2019-01-22 16:11:48 +0100

    add item model

---
+++ db/migrate/20190122150702_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :image_url
+      t.text :description
+      t.decimal :price, null: false, precision: 2
+      t.boolean :has_discount, default: false
+      t.boolean :male
+      t.boolean :female
+      t.integer :discount_percentage, default: 0
+      t.belongs_to :category, index: true
+      t.belongs_to :brand, index: true
+      t.timestamps
+    end
+  end
+end
