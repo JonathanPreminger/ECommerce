4fc5f70d3a1700467450ea83f2db05bbfea5bffe
ssimonlp <simon.lepavec@gmail.com>
2019-01-22 21:08:47 +0100

    fix rubocop issues on Cart  model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :bigint(8)        not null, primary key
+#  total      :decimal(, )
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
