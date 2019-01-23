23bba029122e1ad8b03ffef9ce62a141bcd7535a
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-01-23 14:44:23 +0100

    Add devise User and Admin model controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
+  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
