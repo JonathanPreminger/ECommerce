a6743051773870af8c3b8af8013fc4bbf7860f8d
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-01-23 17:55:23 +0100

    Add associations and associations validations in model for User and Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,4 +5,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_one :cart, dependent: :destroy
+  validates :cart, presence: true
+  has_many :orders, through: :cart
 end
