3f4c84da27480730bd748f405e7ab9be5bd24ad8
ssimonlp <simon.lepavec@gmail.com>
2019-01-23 09:54:47 +0100

    add validations to cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,4 +15,6 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
   belongs_to :user
+  validates :user, presence: true, on: :create
+  validates :total, presence: true, numericality: { greater_than: 0 }, on: :create
 end
