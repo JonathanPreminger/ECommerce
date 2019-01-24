9cce9ed925c4085b8d3f273f6a19f970a83f57c5
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-01-24 10:24:39 +0100

    Delete wrong association in model User and Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,8 +5,4 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
-  has_one :cart, dependent: :destroy
-  validates :cart, presence: true
-  has_many :orders, through: :cart
 end
