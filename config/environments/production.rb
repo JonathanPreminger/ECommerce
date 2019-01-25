f9aeccb02d0a5a42024ff1e5b832b4618cbbf5b5
Juliette Audema <juliette.audema@gmail.com>
2019-01-25 20:15:52 +0100

    Config redis and maildev in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
