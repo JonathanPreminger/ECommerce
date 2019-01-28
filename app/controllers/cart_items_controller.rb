7966badaaa6b9022b165ddc4aa0580d14e97aa84
ssimonlp <simon.lepavec@gmail.com>
2019-01-28 14:17:37 +0100

    fixes rubocop offenses in cart_items controller

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class CartItemsController < ApplicationController
+  def create
+    if @current_cart.cart_items.include?(@chosen_cart_item)
+      @cart_item = @chosen_cart_item
+      @cart_item.update!(quantity: @cart_item.quantity + 1)
+    else
+      @cart_item = CartItem.create!(cart_id: @current_cart.id, item_id: @chosen_item.id, size_id: @chosen_size.id)
+    end
+  end
+
+  def destroy
+    @cart_item = @chosen_cart_item
+    @cart_item.destroy
+  end
+
+  def add_quantity
+    @cart_item = @chosen_item
+    @cart_item.update!(quantity: @cart_item.quantity += 1)
+  end
+
+  def reduce_quantity
+    @cart_item = @chosen_item
+    @cart_item.update!(quantity: @cart_item.quantity += 1) if @cart_item.quantity > 1
+  end
+
+  protected
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity, :item_id, :cart_id, :size_id)
+  end
+
+  def find_cart_items
+    @chosen_item = Item.find(params[:item_id])
+    @chosen_size = Size.find(params[:size_id])
+    @chosen_cart_item = CartItem.find(params[:id])
+  end
+end
