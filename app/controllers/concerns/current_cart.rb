0e081a0550f20539a4fa82c869425c1a056c7705
ssimonlp <simon.lepavec@gmail.com>
2019-01-29 00:50:50 +0100

    add methods to empty cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -12,7 +12,7 @@ module CurrentCart
     if session[:cart_id]
       @current_cart = Cart.find(session[:cart_id])
     else
-      @current_cart = Cart.create!(user_id: current_user.id)
+      @current_cart = Cart.create!(user_id: current_user.id, total: 0)
       session[:cart_id] = @current_cart.id
     end
   end
