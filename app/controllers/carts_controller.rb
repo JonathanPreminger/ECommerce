daf85c32370bfcfd65ada00378ee11047da83146
ssimonlp <simon.lepavec@gmail.com>
2019-01-28 10:26:31 +0100

    adds destroy and create methods for Cart model

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  def show; end
+  include CurrentCart
+
+  def show
+    @cart = @current_cart
+  end
+
+  def destroy
+    @cart = @current_cart
+    @cart.destroy
+    respond_to do |format|
+      format.js { flash[:notice] = "Votre panier a bien été vidé" }
+    end
+  end
 end
