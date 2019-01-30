10ce2117df99d30dd83d51331b633ee34a6ecd30
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-01-30 18:00:51 +0100

    Add test on mailer on recapitulative_command and out_of_stock on subject origin and destination

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,7 +7,15 @@ class AdminMailer < ApplicationMailer
   #   en.admin_mailer.out_of_stock.subject
   #
   def out_of_stock
-    mail(from: "jonathan.mete@hotmail.fr", to: "jonathan.mete@hotmail.fr", subject: "easy",
+    mail(from: "jonathan.mete@hotmail.fr", to: "jonathan.mete@hotmail.fr", subject: "item out of stock",
+         delivery_method_options: {
+           api_key: Rails.application.credentials.public_mailjet.to_s,
+           secret_key: Rails.application.credentials.private_mailjet.to_s
+         })
+  end
+
+  def recapitulative_command
+    mail(from: "jonathan.mete@hotmail.fr", to: "jonathan.mete@hotmail.fr", subject: "Votre commande a bien été enregistrée",
          delivery_method_options: {
            api_key: Rails.application.credentials.public_mailjet.to_s,
            secret_key: Rails.application.credentials.private_mailjet.to_s
--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -7,19 +7,31 @@ RSpec.describe AdminMailer, type: :mailer do
     let(:mail) { AdminMailer.out_of_stock }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Out of stock")
+      expect(mail.subject).to eq("item out of stock")
     end
 
     it "renders the destination" do
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq(["jonathan.mete@hotmail.fr"])
     end
 
     it "renders the origin" do
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["jonathan.mete@hotmail.fr"])
+    end
+  end
+
+  describe "recapitulative_command" do
+    let(:mail) { AdminMailer.recapitulative_command }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Votre commande a bien été enregistrée")
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the destination" do
+      expect(mail.to).to eq(["jonathan.mete@hotmail.fr"])
+    end
+
+    it "renders the origin" do
+      expect(mail.from).to eq(["jonathan.mete@hotmail.fr"])
     end
   end
 end
