d741c4f4310e0a520f1b0ba771a5daca68bd5bf6
ssimonlp <simon.lepavec@gmail.com>
2019-01-30 17:36:29 +0100

    update cart model info

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: carts
 #
 #  id         :bigint(8)        not null, primary key
-#  total      :decimal(, )      default(0.0)
+#  total      :decimal(, )
 #  user_id    :bigint(8)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
