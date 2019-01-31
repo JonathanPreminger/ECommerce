4f7bc2d45c5268e8dfa6087eb39f03e614e455c0
mathieumllt <mathieu.maillet@ymail.com>
2019-01-31 09:43:50 +0100

    Start index of items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
-  def index; end
+  def index
+    @items = Item.all
+    @categories = Category.all
+    @brands = Brand.all
+  end
 
   def show; end
 end
