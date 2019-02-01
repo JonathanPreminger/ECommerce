61a6b3956324bc6802b3ae325398bef4db2f4cc9
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-02-01 12:10:30 +0100

    Change email sender origin

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,7 +7,7 @@ class AdminMailer < ApplicationMailer
   #   en.admin_mailer.out_of_stock.subject
   #
   def out_of_stock
-    mail(from: "jonathan.mete@hotmail.fr", to: "jonathan.mete@hotmail.fr", subject: "item out of stock",
+    mail(from: "juliette.audema@gmail.com", to: "jonathan.mete@hotmail.fr", subject: "item out of stock",
          delivery_method_options: {
            api_key: Rails.application.credentials.public_mailjet.to_s,
            secret_key: Rails.application.credentials.private_mailjet.to_s
@@ -15,7 +15,7 @@ class AdminMailer < ApplicationMailer
   end
 
   def recapitulative_command
-    mail(from: "jonathan.mete@hotmail.fr", to: "jonathan.mete@hotmail.fr", subject: "Votre commande a bien été enregistrée",
+    mail(from: "juliette.audema@gmail.com", to: "jonathan.mete@hotmail.fr", subject: "Votre commande a bien été enregistrée",
          delivery_method_options: {
            api_key: Rails.application.credentials.public_mailjet.to_s,
            secret_key: Rails.application.credentials.private_mailjet.to_s
