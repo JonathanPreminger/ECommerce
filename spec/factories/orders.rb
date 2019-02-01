73fc156390943c33fc6d2e2c67ee1caf17b00774
Juliette Audema <juliette.audema@gmail.com>
2019-02-01 18:57:38 +0100

    Count in header

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -4,16 +4,16 @@
 #
 # Table name: orders
 #
-#  id          :bigint(8)        not null, primary key
-#  total       :decimal(, )
-#  user_id     :bigint(8)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  status      :boolean          default(FALSE)
-#  billing_id  :integer
-#  delivery_id :integer
-#  first_name  :text
-#  last_name   :text
+#  id               :bigint(8)        not null, primary key
+#  total            :decimal(, )
+#  user_id          :bigint(8)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  status           :boolean          default(FALSE)
+#  billing_address  :text
+#  delivery_address :text
+#  first_name       :text
+#  last_name        :text
 #
 
 FactoryBot.define do
