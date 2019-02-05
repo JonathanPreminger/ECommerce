e961e7ddbd4f825664c7ca37170328f6b4fafda2
Juliette Audema <juliette.audema@gmail.com>
2019-02-05 14:28:09 +0100

    Mailer order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,7 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.newest_first
   end
 
   def show
@@ -18,6 +18,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new
     @order = current_user.orders.create(order_params)
+    UserMailer.recap_order(@order.user.email, @order).deliver_later
     redirect_to users_order_path(@order), notice: "Order saved!"
   end
 
